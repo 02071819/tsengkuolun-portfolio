30590c604cfbd09693ef1206eef049916857da31
Tseng Kuo Lun <54053565+02071819@users.noreply.github.com>
2022-05-05 23:39:44 +0800

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,6 +15,7 @@ const Header = () => {
           <h5>Hello I'm</h5>
           <h1>Alan Tseng</h1>
           <h2 className="text-light">Student</h2>
+          <br />
           <h4>Hi ! 我是Alan曾國綸，對Web技術及UI/UX充滿熱情，熱於分享所學。樂觀誠實，持續成長，是我的學習態度。</h4>
           <br />
           <h4>Work hard,be kind and amazing things will happen.</h4>  
