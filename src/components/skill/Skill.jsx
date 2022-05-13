81c81f8dcfe3bcc0bb3e7acb844d238a3b336358
Tseng Kuo Lun <54053565+02071819@users.noreply.github.com>
2022-05-13 17:48:22 +0800

    Update Skill.jsx

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -8,6 +8,8 @@ import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
 
 import "./skill.css";
+import {BiImages} from 'react-icons/bi';
+
 import HTML from "../../assets/html_icon.png";
 import CSS from "../../assets/css_icon.png";
 import JS from "../../assets/js_icon.png";
@@ -22,56 +24,59 @@ import Paper3 from "../../assets/paper3.png";
 import Paper4 from "../../assets/paper4.png";
 import Paper5 from "../../assets/paper5.JPG";
 import Paper6 from "../../assets/paper6.JPG";
-import {IoMdCloseCircleOutline} from "react-icons/io";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const Skill = () => {
-  let data = [
+  let data1 = [
     {
       id: 1,
       Paperimage: Paper1,
-      title: 'CISCO完課證明',
-      content: '資安概論',
+      title: "CISCO完課證明",
+      content: "資安概論",
     },
     {
       id: 2,
-      Paperimage: Paper2,
-      title: 'TQC證照',
-      content: '電子商務概論',
+      Paperimage: Paper3,
+      title: "Hahow完課證明",
+      content: "產品設計實戰",
     },
     {
       id: 3,
-      Paperimage: Paper3,
-      title: 'Hahow完課證明',
-      content: '產品設計實戰',
+      Paperimage: Paper4,
+      title: "參與 TAICHI 2019",
+      content: "大會志工",
     },
     {
       id: 4,
-      Paperimage: Paper4,
-      title: '參與 TAICHI 2019',
-      content: '大會志工',
+      Paperimage: Paper5,
+      title: "DeltaMOOCx證明",
+      content: "智慧商務導論",
     },
     {
       id: 5,
-      Paperimage: Paper5,
-      title: 'DeltaMOOCx完課證明',
-      content: '智慧商務導論',
-    },
-    {
-      id: 6,
       Paperimage: Paper6,
-      title: '台大資訊系統訓練班完課證明',
-      content: 'PHP+MySQL程式設計班',
+      title: "台大資訊系統訓練班證明",
+      content: "PHP+MySQL程式設計班",
+    },
+  ];
+
+  let data2 = [
+    {
+      id: 1,
+      Paperimage: Paper2,
+      title: "TQC證照",
+      content: "電子商務概論",
     },
   ];
 
   const [model, setModel] = useState(false);
-  const [tempingSrc, setTempingSrc] = useState('');
+  const [tempingSrc, setTempingSrc] = useState("");
 
-  const getImg = (Paperimage) =>{
+  const getImg = (Paperimage) => {
     setTempingSrc(Paperimage);
     setModel(true);
-  }
-  
+  };
+
   return (
     <section id="skill">
       <h5>關於技能 & 所學</h5>
@@ -89,6 +94,8 @@ const Skill = () => {
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
+        data-aos="fade-up"
+        data-aos-duration="2000"
       >
         <div className="container graphbox-container">
           <SwiperSlide className="graphbox">
@@ -133,34 +140,51 @@ const Skill = () => {
         </div>
       </Swiper>
 
-       <Swiper
-        slidesPerView={2.5}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper2"
-        data-aos="fade-up" data-aos-duration="2000"
-      >
-        <div className={model? "model open" : "model"}>
-          <img src={tempingSrc} />
-          <IoMdCloseCircleOutline onClick={() => setModel(false)} className="close"/>
-        </div>
+      <div className={model ? "model open" : "model"}>
+        <img src={tempingSrc} />
+        <IoMdCloseCircleOutline
+          onClick={() => setModel(false)}
+          className="close"
+        />
+      </div>
 
+      <div className="skillboard">
         <div className="graphbox2-container">
-          {data.map(({ id, Paperimage, title, content }) => {
+        <h5>自我學習 & 進修</h5>
+        <h2>My Learning</h2>
+        <br />
+          {data1.map(({ id, Paperimage, title, content }) => {
             return (
               <article key={id}>
-                <SwiperSlide className="graphbox2">
-                  <img src={Paperimage} onClick={() => getImg(Paperimage)}/>
-                  <h3>{title}<br />{content}</h3>
-                </SwiperSlide>
+                <div className="graphbox2">
+                  {/* <img src={Paperimage}/> */}
+                  <h3 onClick={() => getImg(Paperimage)}>
+                    <BiImages /> {title} {content} 
+                  </h3>
+                </div>
               </article>
             );
           })}
         </div>
-      </Swiper>
+
+        <div className="graphbox2-container">
+        <h5>證照</h5>
+        <h2>My certificate</h2>
+        <br />
+          {data2.map(({ id, Paperimage, title, content }) => {
+            return (
+              <article key={id}>
+                <div className="graphbox2">
+                  {/* <img src={Paperimage}/> */}
+                  <h3 onClick={() => getImg(Paperimage)}>
+                  <BiImages /> {title} {content}
+                  </h3>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
 
       {/* <div className="container box-container">
         <div className="box">
