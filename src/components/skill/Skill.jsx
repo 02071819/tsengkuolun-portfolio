7ab347bd5f947767a9b3a53569ceb47da6b88c1c
Tseng Kuo Lun <54053565+02071819@users.noreply.github.com>
2022-05-17 00:39:57 +0800

    Update Skill.jsx

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -24,7 +24,7 @@ import Paper3 from "../../assets/paper3.png";
 import Paper4 from "../../assets/paper4.png";
 import Paper5 from "../../assets/paper5.JPG";
 import Paper6 from "../../assets/paper6.PNG";
-import Paper7 from "../../assets/paper7.PNG";
+import Paper7 from "../../assets/paper7.png";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const Skill = () => {
