2b3862ee6b72ae1104f8da4fa4417be0d25f56a6
Tseng Kuo Lun <54053565+02071819@users.noreply.github.com>
2023-12-17 16:15:16 +0800

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/%E5%9C%8B%E7%B6%B8-%E6%9B%BE-1bb6071a6/" ><BsLinkedin size='30px'/></a>
+        <a href="https://www.linkedin.com/in/alan-tseng-1bb6071a6/" ><BsLinkedin size='30px'/></a>
         <a href="https://www.facebook.com/profile.php?id=100006004980674" ><BsFacebook size='30px'/></a>
         <a href="https://github.com/02071819" ><BsGithub size='30px'/></a>
     </div>
